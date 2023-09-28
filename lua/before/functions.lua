e35e5f9eaedeae214702a418fd05a41fd681e239
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-09-28 15:46:56 -0700

    moved to new directories

--- lua/before/functions.lua
+++
@@ -1,8 +0,0 @@
--- ThePrimeagen's function for transparent background in linux term --
-function ColorMyPencils()
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
-  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-end
-
